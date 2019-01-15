cd285ea56e1bb0896e79cbaf762300dd3216ea1d
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2019-01-15 21:51:42 +0100

    Create User schema and Create user mutation

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,6 +22,12 @@ app.use('/graphql', graphqlHTTP({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
@@ -27,12 +35,18 @@ app.use('/graphql', graphqlHTTP({
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(eventInput: EventInput): Event 
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -45,7 +59,7 @@ app.use('/graphql', graphqlHTTP({
             return Event.find()
                 .then(events => {
                     return events.map(event => {
-                        return { 
+                        return {
                             ...event._doc,
                             _id: event._doc._id.toString()
                         }
@@ -61,21 +75,71 @@ app.use('/graphql', graphqlHTTP({
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
                 date: new Date(args.eventInput.date),
+                creator: '62e9569b0ddd050007b20865'
             });
 
+            let createdEvent;
             return event.save()
+                // Save event
                 .then(result => {
-                    return {
+                    createdEvent = {
                         ...result._doc,
                         _id: result._doc._id.toString()
+                    };
+
+                    return User.findById('62e9569b0ddd050007b20865');
+                })
+                // Find user
+                .then(user => {
+                    if (!user) {
+                        throw new Error('User not found.');
                     }
+
+                    user.createdEvents.push(event);
+                    return user.save();
+                })
+                // Update user
+                .then(result => {
+                    return createdEvent;
                 })
                 .catch(error => {
                     console.log(error);
                     throw error
                 });
+        },
+        createUser: (args) => {
 
-            return event;
+            return User.findOne({ email: args.userInput.email })
+                .then(user => {
+                    if (user) {
+                        throw new Error('User exists already.');
+                    }
+
+                    return bcrypt.hash(args.userInput.password, 12)
+                        // Hash password
+                        .then(hashedPassword => {
+
+                            const user = new User({
+                                email: args.userInput.email,
+                                password: hashedPassword
+                            });
+
+                            return user.save();
+
+                        })
+                        // Create user
+                        .then(result => {
+                            return {
+                                ...result._doc,
+                                password: null,
+                                _id: result.id
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        });
+
+                });
         }
     },
     graphiql: true
